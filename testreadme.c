72779b0abf4f97df513f8944acef8e4c4b7d785a
Alma <ahernandez@transact-tech.com>
2023-10-20 15:49:01 -0700

    one more time

--- testreadme.c
+++ testreadme.c
@@ -1,33 +1,109 @@
 #include <stdio.h>
-//Create a read me of first.txt
+#include <stdlib.h>
+// Create a read me of first.txt
+//%p is pointers
+//%d is for integers
+// scanf() reading input from the user or from a file and starting it in variables
+// feof() function to read end of file
+// fgetc function to read character in a file stream
+// fscanf to read a string from the file and then use sscanf to convert the string to an integer.
+
 int main()
 {
-    FILE *fp;
+	FILE *fp;
+	char character;
+	// pointer stores value in first.txt and writes
+	fp = fopen("first.txt", "r");
+	int TwoByThree[2][3];
+	int ThreeByTwo[3][2];
+	int index = 0;
+	int value = 0;
 
-    //pointer stores value in first.txt and writes
-    fp = fopen("first.txt", "r");
+	// check if file is open
+	if (fgetc(fp) != EOF)
+	{
+		printf("File read successfully, hurrah!");
+		printf("\n");
+		// use fscanf and sscanf
+		for (int i = 0; i < 2; i++)
+			for (int j = 0; j < 3; j++)
+			{
+				if (fscanf(fp, " %c", &character) == 1 && character >= '1' && character <= '3')
+				{
+					TwoByThree[i][j] = character - '0';
+					index++;
+				}
+			}
+		printf("The value of index is:%d", index);
+		fseek(fp, index, SEEK_SET);
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 2; j++)
+			{
+				if (fscanf(fp, " %c", &character) == 1 && character >= '1' && character <= '3')
+				{
+					ThreeByTwo[i][j] = character - '0';
+				}
+			}
+		}
+		printf("\nArray 2x3:\n");
+		// display values returned -------- for both arrays
+		for (int i = 0; i < 2; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				printf("%d ", (TwoByThree[i][j]));
+			}
+			printf("\n");
+		}
+		printf("\nArray 3x2:\n");
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 2; j++)
+			{
+				//this one is not putting out correct value for some reason
+				printf("%d ", ThreeByTwo[i][j]);
+			}
+			printf("\n");
+		}
+	}
+		// didn't need this afterall 
+		// get the size of the row, as it is the only difference between the ThreeByTwo or the TwoByTwo
+		// printf("Size of row int in twobythree is: %d", sizeof(TwoByThree[0]));
+		// printf("\n");
+		// int value2;
+		// //int is 4 bytes
+		// printf("Size of value: %d", sizeof(value2));
+		// printf("\n");
+		// // math parth, should result in 6 solutions
+		// printf("Size of row int in threebytwo is: %d", sizeof(ThreeByTwo[0]));
+		// printf("\n");
+		
+		int result[2][3];
 
-    //check if file is open
-    if(fp == NULL)
-    {
-        printf("Could not locate file.");
-    }
-    else
-        printf("File read successfully, hurrah!");
-        scanf("This is the value %d", &fp);
-    
-        // Two matrices : Examples below
-        // First is 2x3: 2 2
-        //               2 2
-        //               2 2
-        // Next is 3x2:  2 2 2
-        //               2 2 2
-
-        //assume these lines are providing two matrices with the given rows and columns and data.
-        //Find the multiplication of these two matrices and add the result below the last line in the input file (don't remove its contents).
-
-        // so create two matrices w the given line, not sure which order but I'mma go with 
-        int temp = 0;
-
-    return 0;
+		printf("\n");
+		for(int i=0; i < 2; i++)
+		{
+			for(int j=0; j <3; j++)
+			{
+				printf("The current value of TwoByThree is when index j is %d", j);
+				printf(": %d", TwoByThree[i][j]);
+				printf("\n");
+				printf("The current value of ThreeByTwo whem index i is %d", i); 
+				printf(": %d", ThreeByTwo[j][i]);
+				printf("\n");
+				printf("\n");
+				result[i][j] = TwoByThree[i][j] * ThreeByTwo[j][i];
+			}
+		}
+		printf("\nResult value is :\n");
+		//print result
+		for(int i=0; i < 2; i++)
+		{
+			for(int j=0; j <3; j++)
+			{
+				printf("%d ", result[i][j]);
+			}
+		}
+		return 0;
 }
