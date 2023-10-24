2d86b8b79d1383e63623771326a234899d782a4b
alma h <arumahern@gmail.com>
2023-10-24 15:15:57 -0700

    added a new file, made some progress with malloc but still getting garbage in output

--- testreadme.c
+++ testreadme.c
@@ -1,42 +1,45 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX_SIZE 32
 //Create a read me of first.txt
 int main()
 {
     FILE *fp;
-
-    //pointer stores value in first.txt and writes
+    //declare pointer using pointer declaration
+    char *buffer = NULL;
+    char temp;
+    long size;
+    //this address will be used to store 
+    printf("Address of pointer : %p", buffer);
     fp = fopen("first.txt", "r");
 
-    //check if file is open
     if(fp == NULL)
     {
-        printf("Could not locate file.");
+        printf("Something is wrong.");
+        fclose(fp);
     }
-    else
-        printf("File read successfully, hurrah!");
-        scanf("This is the value %d", &fp);
+
+    printf("File was opened successfully.");
+    //fseek(file, offset, SEEK_SET: moves the object pointer to the end of the file)
+    fseek(fp, 0, SEEK_END);
+    //ftell : returns the current file position of the specified stream with respect to the starting of the file
+    //using fseek we should be pointing to the end, and now we can use that as a reference for size of the array 
+    //based on the start of the file
+    size = ftell(fp);
+    printf("Size of file: %ld bytes", size);
+    // allocate memory using malloc()
+    // how to use malloc: ptr = (cast-type*) malloc(byte-size)
+    buffer = (char*)malloc(size);
+    fread(buffer, 1, size, fp);
+    buffer[size] = '\0';
+    printf("File content:\n%s\n", buffer);
+    //fseek(file, offset, SEEK_SET: moves the object pointer to beggining of the file)
+    //reset to begggining so that we can start reading characters
+    fseek(fp, 0, SEEK_SET);
     
-        // Two matrices : Examples below
-        // First is 2x3: 2 2
-        //               2 2
-        //               2 2
-        // Next is 3x2:  2 2 2
-        //               2 2 2
-
-        //assume these lines are providing two matrices with the given rows and columns and data.
-        //Find the multiplication of these two matrices and add the result below the last line in the input file (don't remove its contents).
-
-        // so create two matrices w the given line, not sure which order but I'mma go with 
-        int temp = 0;
-
-        for(int i=0; i < 6; i++);
-        {
-            for(int j= 0; j < 6; j++)
-            {
-             temp = j; 
-             printf("%d", temp);
-            }
-        }
-
-    return 0;
+    //free memory
+    free(buffer);
+    return 0; 
 }
